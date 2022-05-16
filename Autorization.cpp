b638177719b1316fdb026045451dfa82303b4ae9
nuta22 <104190585+nuta22@users.noreply.github.com>
2022-05-16 12:58:56 +0300

    Add files via upload

---
+++ Autorization.cpp
@@ -0,0 +1,29 @@
+﻿#include "Libr.h"
+#include "Autorization.h"
+
+using namespace std;
+
+struct account {
+	char login[16];
+	char password[16];
+};
+bool authorize(char* login = 0, char* password = 0)
+{
+	char login_buf[16];
+	char password_buf[16];
+
+	if (!login)
+	{
+		login = login_buf;
+		std::cout << ("Введите логин:");
+		cin >> ("%15s", login);
+	}
+
+	if (!password)
+	{
+		password = password_buf;
+		std::cout << ("Введите пароль:");
+		cin >> ("%15s", password);
+	}
+	return true;
+}
---
+++ Autorization.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Libr.h"
+#include "Binf.h"
+
+class autorization
+{
+private:
+	string admin;
+	string manager;
+	string users;
+public:
+	void Autorization();
+};
